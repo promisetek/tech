72cf0f622a5e8d0158294fe1cd41f65a7dea66bc
promise iheme <prontolee2000@yahoo.com>
2024-01-28 20:46:59 -0600

    1st commit

---
+++ mlc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "Multi line comment starts"
+<<Promise
+12 things you need to understand.
+1. When you are winning, keep your mouth shut.
+2. Never expect to get what you give not everyone has a heart of flesh.
+3. Don't tell people your plan.Wait until the results to speak for themselves.
+4. Everyday you wake up you have a new job to be better than yesterday.
+5. You will never get what you want until you are grateful for what you have.
+6. Sometimes what you are trying to hold on to is exactly what you should let go.
+7. If you want to buy things without looking at the price, then you must work without looking at the clock.
+8. God puts people in your life for a reason and removes them for a better reason.
+9. The Devil would not be attacking you so hard if there wasn't something value in you.
+10. Thieves don't break into empty houses.
+11. You are going to piss a lot of people off when you start doing what's best for you.
+12. You will lose a lot friends when you get really serious about your life goals.
+12. Life does not get easier, you just get stronger.
+Author: Lion Tiger
+Copyright C Promise Land Technologies
+pwd
+Promise
+echo "Multi line comment ends"
+date
+echo $HOSTNAME
